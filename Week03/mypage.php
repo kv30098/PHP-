f7d4ab3784f3fac10c18eadc74fec928ff368e3a
kv30098 <160998258+kv30098@users.noreply.github.com>
2024-03-20 23:07:22 +0800

    Add files via upload

---
+++ Week03/mypage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+<head>
+    <meta charset="uft-8">
+    <title>我的網頁</title>
+    <link rel=stylesheet type="text/css" href="style.css">
+</head>
+<body>
+    <div class="center">
+        <h1><font color=#c2ddec>我的第一個網頁</font></h1>
+    </div>
+    <hr class="divider">
+    <br/>
+    <div class="content-block">
+        <div class="container">
+            <div class="center">
+            <img src="img/sky.jpg" width="50%" height="50%" alt="星空照片">
+            <h3>這是一張傍晚的雲海圖</h3>
+
+            <a href="#">返回頂部</a>
+            </div>
+        </div><br/>
+    </div>
+</body>
+</html>
