e87415f28d5f39bd7c9e51a3c001b4ec828a4d53
kv30098 <160998258+kv30098@users.noreply.github.com>
2024-04-17 22:26:10 +0800

    Add files via upload

--- Week06/party/enroll.php
+++ Week06/party/enroll.php
@@ -7,28 +7,28 @@
 <form action="result.php" method="post">
 <body>
     <div class="center">
-        <h1>高大資管系報名晚會</h1>
+        <h1>高大資管系晚會報名系統</h1>
     </div>
     <hr class="divider">
     <div class="center">
         <h3>
-        姓名：<input type="text" name="sName" value=""><br/>
-        學號：<input type="text" name="sStuId" value=""><br/>
+        姓名：<input type="text" name="sName" value="" required><br/>
+        學號：<input type="text" name="sStuId" value="" required><br/>
         年級：
-        <input type="radio" name="sGrade" value="freshman">大一
-        <input type="radio" name="sGrade" value="sophomore">大二
-        <input type="radio" name="sGrade" value="junior">大三
-        <input type="radio" name="sGrade" value="senior">大四
+        <input type="radio" name="sGrade" value="大一">大一
+        <input type="radio" name="sGrade" value="大二">大二
+        <input type="radio" name="sGrade" value="大三">大三
+        <input type="radio" name="sGrade" value="大四">大四
         <br/>
         性別：
-        <input type="radio" name="sGender" value="male">男生
-        <input type="radio" name="sGender" value="Female">女生
+        <input type="radio" name="sGender" value="男生">男生
+        <input type="radio" name="sGender" value="女生">女生
         </br>
         飲料偏好：
-        <input type="checkbox" name="sDrink" value="soda">汽水
-        <input type="checkbox" name="sBaverage" value="alcohol">調酒
-        <input type="checkbox" name="sBaverage" value="juice">果汁
-        <input type="checkbox" name="sBaverage" value="tea">茶類
+        <input type="checkbox" name="sBaverage[]" value="汽水">汽水
+        <input type="checkbox" name="sBaverage[]" value="調酒">調酒
+        <input type="checkbox" name="sBaverage[]" value="果汁">果汁
+        <input type="checkbox" name="sBaverage[]" value="茶類">茶類
         <br/>
         想要的組別顏色偏好：
         <input type="color" name="sColor" value=""><br/>
@@ -36,10 +36,11 @@
         <input type="number" name="sNumber" step="1" min="0" max="4" value=""><br/><br/>
         外系報名者系所：
         <select name="sDepartment" multiple>
-            <option value="ABA">亞太系
-            <option value="AE">應金系
-            <option value="FI">金管系
-            <option value="WLL">西語系
+            <option value="A">亞太系
+            <option value="B">應金系
+            <option value="C">金管系
+            <option value="D">西語系
+            <option value="無">無
         </select><br/>
         其他附註：
         <textarea name="sComment" value="" rows="5" cols="20"></textarea><br/>
