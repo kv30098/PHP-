a2db22d66d875ceae4a22e46cb66d0eeb62f072b
kv30098 <160998258+kv30098@users.noreply.github.com>
2024-06-16 01:43:34 +0800

    Add files via upload

---
+++ week13-發送email/email.php
@@ -0,0 +1,13 @@
+<meta charset="utf-8">
+
+<form action="sendemail.php" method="POST">
+    收件人信箱：<input type="email" name="to"><br>
+    郵件標題：<input type="text" name="subject"><br>
+    郵件內容：
+    <textarea rows="30" cols="30" name="content">
+    
+    </textarea><br>
+
+    <input type="submit">
+
+</form>
